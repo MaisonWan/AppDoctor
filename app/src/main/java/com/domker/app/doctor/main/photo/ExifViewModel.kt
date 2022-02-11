e28271eff88d2d7168800431d600b03b4b6c25bc
maisonwan <maisonwan@gmail.com>
2022-02-11 14:15:57 +0800

    change some deprecated

--- app/src/main/java/com/domker/app/doctor/main/photo/ExifViewModel.kt
+++ app/src/main/java/com/domker/app/doctor/main/photo/ExifViewModel.kt
@@ -1,7 +1,12 @@
 package com.domker.app.doctor.main.photo
 
+import android.app.Activity
+import android.net.Uri
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.domker.app.doctor.util.DataFormat
+import com.domker.base.thread.AppExecutors
 
 /**
  *
@@ -9,5 +14,55 @@ import androidx.lifecycle.ViewModel
  */
 class ExifViewModel : ViewModel() {
 
-    val exif = MutableLiveData<PhotoExif>()
+    private val exif = MutableLiveData<List<ExifItem>>()
+
+    fun getExif(): LiveData<List<ExifItem>> = exif
+
+    /**
+     * 异步读取指定位置的图片，并且分析EXIF信息
+     */
+    fun parserExif(activity: Activity, uri: Uri) {
+        AppExecutors.executor.execute {
+            val stream = activity.contentResolver.openInputStream(uri)
+            val parser = PhotoExifParser(stream!!)
+            val photoExif = parser.loadExif()
+            exif.postValue(convert(photoExif))
+        }
+    }
+
+    /**
+     * 将Exif信息的实体，转化为KV的值
+     */
+    private fun convert(exif: PhotoExif): List<ExifItem> {
+        val data = mutableListOf<ExifItem>()
+        data.add(ExifItem("EXIF版本", exif.exifVersion))
+        data.add(ExifItem("分辨率", "${exif.width} x ${exif.height}px"))
+        data.add(ExifItem("分辨率单元", exif.resolutionUnit.toString()))
+        data.add(ExifItem("X像素密度", exif.xResolution.toString()))
+        data.add(ExifItem("Y像素密度", exif.yResolution.toString()))
+
+        data.add(ExifItem("软件名称", exif.software))
+        data.add(ExifItem("拍摄时间", DataFormat.getFormatFullDate(exif.camera.dateTime)))
+        data.add(ExifItem("数字化时间", DataFormat.getFormatFullDate(exif.camera.dateTimeDigitized)))
+        data.add(ExifItem("设备型号", exif.camera.model))
+        data.add(ExifItem("设备品牌", exif.camera.make))
+        data.add(ExifItem("焦距", "${exif.camera.focalLength} mm"))
+        data.add(ExifItem("测光模式", DataFormat.meteringMode(exif.camera.meteringMode)))
+        data.add(ExifItem("光圈", "f/${exif.camera.aperture}"))
+        data.add(ExifItem("曝光时长", DataFormat.getDoubleShort(exif.camera.exposureTime) + "s"))
+        data.add(ExifItem("曝光补偿", exif.camera.exposureBiasValue.toString()))
+        data.add(ExifItem("曝光模式", DataFormat.exposureMode(exif.camera.exposureProgram)))
+        data.add(ExifItem("ISO感光度", exif.camera.ios.toString()))
+        data.add(ExifItem("色彩空间", DataFormat.colorSpace(exif.colorSpace)))
+
+        data.add(ExifItem("白平衡", exif.camera.whiteBalance))
+        data.add(ExifItem("闪光灯", exif.camera.flash.toString()))
+        data.add(ExifItem("旋转角度", "${exif.camera.orientation}°"))
+        data.add(ExifItem("GPS定位", "", TYPE_LOCATION, exif.gps))
+
+        data.add(ExifItem("GPS时间", DataFormat.getFormatFullDate(exif.gps.gpsDateTime)))
+        data.add(ExifItem("海拔高度", "${exif.gps.altitudeRef}${exif.gps.altitude} M"))
+
+        return data
+    }
 }
--- app/src/main/java/com/domker/app/doctor/main/photo/PhotoAnalysisFragment.kt
+++ app/src/main/java/com/domker/app/doctor/main/photo/PhotoAnalysisFragment.kt
@@ -3,18 +3,16 @@ package com.domker.app.doctor.main.photo
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.domker.app.doctor.R
 import com.domker.app.doctor.databinding.FragmentMainPhotoBinding
-import com.domker.app.doctor.util.DataFormat
 import com.domker.app.doctor.widget.BaseAppFragment
 import com.domker.base.addDividerItemDecoration
-import com.domker.base.thread.AppExecutors
-
-const val ACTION_CHOOSE_PHOTO = 200
 
 /**
  * 展示照片分析EXIF信息的地方
@@ -25,6 +23,7 @@ class PhotoAnalysisFragment : BaseAppFragment() {
     private lateinit var adapter: ExifListAdapter
     private lateinit var binding: FragmentMainPhotoBinding
     private lateinit var exifViewModel: ExifViewModel
+    private lateinit var launcher: ActivityResultLauncher<Intent>
 
     // 是否已经初始化了图片展示的View
     private var isInitPhotoView = false
@@ -39,7 +38,7 @@ class PhotoAnalysisFragment : BaseAppFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        exifViewModel = ViewModelProvider(this).get(ExifViewModel::class.java)
+        exifViewModel = ViewModelProvider(this)[ExifViewModel::class.java]
 
         binding = FragmentMainPhotoBinding.inflate(inflater, container, false)
         return binding.root
@@ -48,6 +47,11 @@ class PhotoAnalysisFragment : BaseAppFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initButton()
+
+        launcher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                parsePhotoExif(result.resultCode, result.data)
+            }
     }
 
     private fun initButton() {
@@ -55,7 +59,7 @@ class PhotoAnalysisFragment : BaseAppFragment() {
             openPhoto()
         }
 
-        exifViewModel.exif.observe(viewLifecycleOwner) {
+        exifViewModel.getExif().observe(viewLifecycleOwner) {
             showExif(it)
         }
     }
@@ -102,65 +106,26 @@ class PhotoAnalysisFragment : BaseAppFragment() {
         intent.type = "image/*"
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
         intent.action = Intent.ACTION_GET_CONTENT  //实现相册多选
-        startActivityForResult(Intent.createChooser(intent, "选择图片"), ACTION_CHOOSE_PHOTO)
+        launcher.launch(Intent.createChooser(intent, "选择图片"))
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == ACTION_CHOOSE_PHOTO && data != null) {
+    private fun parsePhotoExif(resultCode: Int, data: Intent?) {
+        if (data != null) {
             initPhotoView()
-            val uri = if (data.data != null) {
-                data.data
-            } else {
-                data.clipData?.getItemAt(0)?.uri
-            }
+            val uri = data.data ?: data.clipData?.getItemAt(0)?.uri
             uri?.let {
                 Glide.with(requireContext()).load(it).into(binding.imageViewPreview)
-                AppExecutors.executor.execute {
-                    val stream = requireActivity().contentResolver.openInputStream(it)
-                    val parser = PhotoExifParser(stream!!)
-                    val exif = parser.loadExif()
-                    exifViewModel.exif.postValue(exif)
-                }
+                // 异步解析图片的信息
+                exifViewModel.parserExif(requireActivity(), it)
             }
-
         }
     }
 
     /**
      * 展示信息
      */
-    private fun showExif(exif: PhotoExif) {
-        val data = mutableListOf<ExifItem>()
-        data.add(ExifItem("EXIF版本", exif.exifVersion))
-        data.add(ExifItem("分辨率", "${exif.width} x ${exif.height}px"))
-        data.add(ExifItem("分辨率单元", exif.resolutionUnit.toString()))
-        data.add(ExifItem("X像素密度", exif.xResolution.toString()))
-        data.add(ExifItem("Y像素密度", exif.yResolution.toString()))
-
-        data.add(ExifItem("软件名称", exif.software))
-        data.add(ExifItem("拍摄时间", DataFormat.getFormatFullDate(exif.camera.dateTime)))
-        data.add(ExifItem("数字化时间", DataFormat.getFormatFullDate(exif.camera.dateTimeDigitized)))
-        data.add(ExifItem("设备型号", exif.camera.model))
-        data.add(ExifItem("设备品牌", exif.camera.make))
-        data.add(ExifItem("焦距", "${exif.camera.focalLength} mm"))
-        data.add(ExifItem("测光模式", DataFormat.meteringMode(exif.camera.meteringMode)))
-        data.add(ExifItem("光圈", "f/${exif.camera.aperture}"))
-        data.add(ExifItem("曝光时长", DataFormat.getDoubleShort(exif.camera.exposureTime) + "s"))
-        data.add(ExifItem("曝光补偿", exif.camera.exposureBiasValue.toString()))
-        data.add(ExifItem("曝光模式", DataFormat.exposureMode(exif.camera.exposureProgram)))
-        data.add(ExifItem("ISO感光度", exif.camera.ios.toString()))
-        data.add(ExifItem("色彩空间", DataFormat.colorSpace(exif.colorSpace)))
-
-        data.add(ExifItem("白平衡", exif.camera.whiteBalance))
-        data.add(ExifItem("闪光灯", exif.camera.flash.toString()))
-        data.add(ExifItem("旋转角度", "${exif.camera.orientation}°"))
-        data.add(ExifItem("GPS定位", "", TYPE_LOCATION, exif.gps))
-
-        data.add(ExifItem("GPS时间", DataFormat.getFormatFullDate(exif.gps.gpsDateTime)))
-        data.add(ExifItem("海拔高度", "${exif.gps.altitudeRef}${exif.gps.altitude} M"))
-
-        adapter.setData(data)
-        adapter.notifyDataSetChanged()
+    private fun showExif(list: List<ExifItem>) {
+        adapter.setData(list)
+        adapter.notifyItemRangeChanged(0, list.size)
     }
 }
